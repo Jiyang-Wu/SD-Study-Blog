91ca701e45334fd79e685dff91f180e1a97e4c35
Jiyang Wu <jiyangwuovo@gmail.com>
2021-04-05 18:20:58 -0700

    finishing setting up

--- server/index.js
+++ server/index.js
@@ -2,9 +2,26 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTIONURL =
+    "mongodb+srv://JiyangWu:Jiyang@1218@cluster0.1zagb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+mongoose
+    .connect(CONNECTIONURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`))
+    )
+    .catch((error) => {
+        console.log(error);
+    });
+
+mongoose.set("useFindAndModify", false);
